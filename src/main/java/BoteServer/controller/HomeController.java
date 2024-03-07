2f3190a729a7a5a9349db8d41a9db44d0d15b8e3
Unerkant <freiwifi@gmail.com>
2024-03-07 15:42:30 +0100

    1. version 0.0.4 2 Server lauzeit bearbeitet 3.Quelle: Service/RuntimeService.java

--- src/main/java/BoteServer/controller/HomeController.java
+++ src/main/java/BoteServer/controller/HomeController.java
@@ -1,6 +1,6 @@
 package BoteServer.controller;
 
-import BoteServer.utilities.Methoden;
+import BoteServer.service.RuntimeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,20 +14,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     @Autowired
-    private Methoden methoden;
+    private RuntimeService runtimeService;
 
 
-    /**
-     * Home Anzeigen + Tomcat Serverzeit anzeige
-     *
-     * @param model
-     * @return
-     */
     @GetMapping(value = {"/", "/home"})
     public String home(Model model) {
 
-        // Tomcat Serverzeit anzeige in footer
-        model.addAttribute("zeitstempel", methoden.zeitstempel());
+        // Server laufzeit in footer anzeigen
+        model.addAttribute("zeitstempel", runtimeService.getRuntimeSinceStart());
 
         return "home";
     }
--- src/main/java/BoteServer/utilities/Methoden.java
+++ src/main/java/BoteServer/utilities/Methoden.java
@@ -9,37 +9,6 @@ import org.springframework.stereotype.Component;
 @Component
 public class Methoden {
 
-
-    /**
-     * Tomcat laufzeit anzeigen, nur Tagen Stunden und Minuten
-     *
-     */
-    private long startZeit = System.currentTimeMillis(); //Nur zum Testen: System.currentTimeMillis() * 6;
-    public String zeitstempel(){
-
-        long endeZeit =  System.currentTimeMillis();
-        long diffMillis = Math.abs(startZeit - endeZeit);
-
-        int timeInSekunden = ((int)diffMillis) / 1000;
-        //int sekunden = timeInSekunden % 60;
-        int timeInMinuten = timeInSekunden / 60;
-        int minuten = timeInMinuten % 60;
-        int timeInStunden = timeInMinuten / 60;
-        int stunden = timeInStunden % 24;
-        int timeInTagen = timeInStunden / 24;
-        int tagen = timeInTagen % 24;
-
-        String minutenZeigen = minuten > 0 ? minuten
-                + (minuten > 1 ? " Minuten " : " Minute ") : "";
-        String stundenZeigen = stunden > 0 ? stunden
-                + (stunden > 1 ? " Stunden " : " Stunde ") : "";
-        String tagenZeigen = tagen > 0 ? tagen
-                + (tagen > 1 ? " Tagen " : " Tag ") : "";
-
-        String textZeigen = minuten > 0 ? "Tomcat Server läuft seit" : "From Bote";
-        String komplektZeigen =  textZeigen + " " + tagenZeigen +" " + stundenZeigen + " " + minutenZeigen;
-
-        return  komplektZeigen;
-    }
+    // noch keine hilfs Methoden
 
 }
